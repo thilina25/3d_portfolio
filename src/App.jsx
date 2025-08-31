d2078dc69edf1aabfba5f3c8e8903ae204ae3efe
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2025-08-31 08:48:42 +0530

    Testimonail section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Education from './sections/Education';
 import Hero from './sections/Hero';
 import NavBar from './sections/NavBar';
 import Project from './sections/Project';
+import Testimonail from './sections/Testimonail';
 
 const App = () => {
   return <div className='container mx-auto 
   max-w-7xl'>
-    <NavBar/>
-    <Hero/>
-    <About/>
-    <Project/>
+    <NavBar />
+    <Hero />
+    <About />
+    <Project />
     <Education />
+    <Testimonail />
     <section className="min-h-screen"/>
     <section className="min-h-screen"/>
     <section className="min-h-screen"/>
